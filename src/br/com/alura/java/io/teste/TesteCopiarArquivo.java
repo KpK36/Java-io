0f1f84e180d9760ecc6c49e2663a6602db937cbe
KpK36 <kayopatryck1@gmail.com>
2023-01-13 22:51:51 -0300

    Entrada de dados pelo teclado

--- src/br/com/alura/java/io/teste/TesteCopiarArquivo.java
+++ src/br/com/alura/java/io/teste/TesteCopiarArquivo.java
@@ -5,26 +5,28 @@ import java.io.BufferedWriter;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 
 public class TesteCopiarArquivo {
 
 	public static void main(String[] args) throws IOException {
 
-		FileInputStream file = new FileInputStream("teste.txt");
+		InputStream file = System.in;
 		InputStreamReader input = new InputStreamReader(file);
 		BufferedReader br = new BufferedReader(input);
 		
 		
-		FileOutputStream fileOutput = new FileOutputStream("novoTeste.txt");
+		OutputStream fileOutput = new FileOutputStream("novoTeste.txt");
 		OutputStreamWriter output = new OutputStreamWriter(fileOutput);
 		BufferedWriter bw = new BufferedWriter(output);
 		
 		
 		String linha = br.readLine();
 		
-		while(linha != null) {			
+		while(linha != null && !linha.isEmpty()) {			
 			bw.write(linha);	
 			bw.newLine();
 			linha = br.readLine();
